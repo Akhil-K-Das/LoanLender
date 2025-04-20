0a5079b0940b7408d8a22c8f82b3b58466af5c03
AKHIL K N <akhilkn4@gmail.com>
2025-04-20 13:22:02 +0530

    added a condition to repay loan if user wallet balance exceed

--- app/jobs/interest_calculator_job.rb
+++ app/jobs/interest_calculator_job.rb
@@ -5,9 +5,13 @@ class InterestCalculatorJob < ApplicationJob
     puts "********* InterestCalculatorJob is running..."
 
     Loan.open.find_each do |loan|
-      interest = (loan.amount * loan.interest_rate / 100.0) / (365 * 24 * 12)
-      puts "Loan ##{loan.id} - Calculated interest: #{interest.round(2)}"
+      interest = (loan.amount * loan.interest_rate / 100.0) # / (365 * 24 * 12)
+      puts "Loan ##{loan.id} - Calculated interest: #{interest.round(2)} and balance: #{loan.user.wallet.balance}"
       loan.update!(amount: loan.amount + interest)
+      if loan.amount >= loan.user.wallet.balance
+        puts "Loan ##{loan.id} - Amount is greater than user's wallet balance. Proceeding to repayment........"
+        LoanService.new(loan).repay!
+      end
     end
   end
 end
